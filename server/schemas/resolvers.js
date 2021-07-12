41e332400b083a292d1d5a44445c25dc6ce90c83
Juawito <santiagojuan311@gmail.com>
2021-07-12 15:38:02 -0400

    creating query and mutations for backend gql endpoint

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,25 +1,18 @@
 const { User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
-
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-                const user = await User.findById(context.user._id);
+                const user = await User.findById(context.user._id).populate('savedBooks');
                 return user;
             }
             throw new AuthenticationError('Not logged In');
         }
     },
     Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-
-            return { token, user };
-        },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
             if (!user) {
@@ -33,9 +26,15 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
         saveBook: async (parent, args, context) => {
             if (context.user) {
-                const saveBookToUser = User.findByIdAndUpdate({ _id: context.user.id },
+                const saveBookToUser = User.findByIdAndUpdate({ _id: context.user._id },
                     {
                         $addToSet: { savedBooks: args }
                     },
@@ -45,11 +44,11 @@ const resolvers = {
             }
             throw new AuthenticationError('Not Logged In!');
         },
-        removeBook: async (parent, args, context) => {
+        removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
-                const removeBookFromUser = User.findByIdAndUpdate({ _id: context.user.id },
+                const removeBookFromUser = User.findByIdAndUpdate({ _id: context.user._id },
                     {
-                        $pull: { savedBooks: { bookId: args } }
+                        $pull: { savedBooks: { bookId } }
                     },
                     { new: true }
                 );
